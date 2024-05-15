71d59a9af5b9cb37d87f5d1b2de23c1081d50fcd
kalkidan-solomon-coder <152845891+kalkidan-solomon-coder@users.noreply.github.com>
2024-05-15 23:21:36 +0300

    first commit

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar"
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <Navbar/>
+      
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ e-commerce/src/Navbar.jsx
@@ -0,0 +1,22 @@
+import { PiShoppingCartBold } from "react-icons/pi";
+import { FaRegHeart } from "react-icons/fa";
+import { GoPersonAdd } from "react-icons/go";
+import { TiShoppingCart } from "react-icons/ti";
+
+function Navbar(){
+    return (
+        <>
+        <div>
+            <div>
+                <div>
+                    </>
+                </div>
+                <></>
+                <></>
+            </div>
+        </div>
+        </>
+    )
+
+}
+export default Navbar
